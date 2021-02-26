612034f710ee5524aeff6062042c90c47cd57b5d
MKmacky <mkazuma529@icloud.com>
2021-02-26 12:13:24 +0900

    メッセージ投稿機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,5 @@
 class Message < ApplicationRecord
 
-  t.string  :content
-  t.references :room, foreign_key: true
-  t.references :user, foreign_key: true
-
   belongs_to :room
   belongs_to :user
 
